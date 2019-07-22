cca841b5732c5410cf418eacc46357440b72d5bd
deividfoggi <39340082+deividfoggi@users.noreply.github.com>
2019-07-22 14:04:14 -0300

    Changed the input from a direct Get-Mailbox cmd-let to a variable named $inputObject. Also added commands in the end for session clean up.

--- Get-O365AcceptedDomainByProxyAddresses.ps1
+++ Get-O365AcceptedDomainByProxyAddresses.ps1
@@ -28,11 +28,13 @@
 # 
 #########################################################################################################################
 
-
+$inputObject = Get-Mailbox -ResultSize Unlimited -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
 Import-PSSession (New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Authentication Basic -Credential $global:Credential -SessionOption (New-PSSessionOption -SkipRevocationCheck -SkipCACheck -SkipCNCheck)  -AllowRedirection) -Prefix O365 -AllowClobber | Out-Null
 $arr = @()
-(get-mailbox -resultsize unlimited).emailaddresses | ?{$_.PrefixString -eq "smtp"} | select SmtpAddress | %{$addr = $_.SmtpAddress.Split("@");$arr += $addr[1]}
-$domains = $arr | select -unique
+($inputObject).emailaddresses | Where-Object{$_.PrefixString -eq "smtp"} | Select-Object SmtpAddress | ForEach-Object{$addr = $_.SmtpAddress.Split("@");$arr += $addr[1]}
+$domains = $arr | Select-Object -Unique
 $arr = @()
-$domains | %{If(Get-O365AcceptedDomain $_ -ErrorAction SilentlyContinue){$obj = New-Object PSObject;$obj | Add-Member -Value $_ -Name "Accepted Domain" -MemberType NoteProperty;$obj | Add-Member -Value "Found" -Name "Status" -MemberType NoteProperty;$arr += $obj}else{$obj = New-Object PSObject;$obj | Add-Member -Value $_ -Name "Accepted Domain" -MemberType NoteProperty;$obj | Add-Member -Value "Not Found" -Name "Status" -MemberType NoteProperty;$arr += $obj}}
+$domains | Where-Object{If(Get-O365AcceptedDomain $_ -ErrorAction SilentlyContinue){$obj = New-Object PSObject;$obj | Add-Member -Value $_ -Name "Accepted Domain" -MemberType NoteProperty;$obj | Add-Member -Value "Found" -Name "Status" -MemberType NoteProperty;$arr += $obj}else{$obj = New-Object PSObject;$obj | Add-Member -Value $_ -Name "Accepted Domain" -MemberType NoteProperty;$obj | Add-Member -Value "Not Found" -Name "Status" -MemberType NoteProperty;$arr += $obj}}
 $arr
+
+Get-PSSession | Remove-PSSession -Confirm:$false
